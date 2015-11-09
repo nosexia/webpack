19415fd58da27ae54da8eaf07ef46ff1a5ff6776
409903113@qq.com <409903113@qq.com>
2015-11-09 16:19:39 +0800

    js文件引入node内置模块url

--- scripts/indexMain.js
+++ scripts/indexMain.js
@@ -1,6 +1,11 @@
 define([
-    './deps'
-],function(deps){
-    console.log(deps);
+],function(){
+    //webpack打包，js文件引入node内置模块'url'
+    var url = require('url');
+    var urlResult = url.parse('http://webpack.toobug.net/zh-cn/index.html');
+    //url对应parse方法，解析urlResult字符串
+    console.log(url.parse(urlResult));
 });
+
+
 
