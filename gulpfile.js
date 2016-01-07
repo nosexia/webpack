b7995150504d12797c743217539687073c731606
nose <nose@futunn.com>
2016-01-07 10:59:00 +0800

    引入webpack.config.js配置文件

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,7 @@ var named = require('vinyl-named');
 // 引入webpack模块
 var webpack = require('webpack-stream');
 
-
-// 暂时不懂
-var devConfig = {
-    output : {
-        filename : '[name].js'
-    },
-    /*
-    module : {
-        loaders : [
-            {test : /\.html$/, loader : 'text'}
-        ]
-    },
-   /* devtool: '#source-map'*/
-};
-
-devConfig = require('./webpack.config.js');
+var devConfig = require('./webpack.config.js');
 
 
 gulp.task('dev', function(){
